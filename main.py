5cec666bbe938583ab822888da15fc2d82cacc2a
Elyse Kriegel <esamkriegel@gmail.com>
2020-12-09 01:47:07 +0000

    'Stopped multi-threading the driver"

--- main.py
+++ main.py
@@ -1,70 +1,88 @@
 import requests
+from requests.structures import CaseInsensitiveDict
 import time
 from urllib.parse import urljoin
-from multiprocessing.pool import ThreadPool, Pool
+from multiprocessing import Pool
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import threading
 import os
 
-class Driver:
-    def __init__(self):
-        options = webdriver.ChromeOptions()
-        options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--headless")
-        self.driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),options=options)
+options = webdriver.ChromeOptions()
+#options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
+options.add_argument("--headless")
+driver = webdriver.Chrome(options=options)
 
-    def __del__(self):
-        self.driver.quit() # clean up driver when we are cleaned up
-        print('The driver has been "quitted".')
+def post_product(data):
+  url = "https://warm-ridge-24483.herokuapp.com/product"
+
+  headers = CaseInsensitiveDict()
+  headers["Content-Type"] = "application/json"
+
+  data = data
+  resp = requests.post(url, headers=headers, json=data)
+
+  print(resp.status_code)
 
 def get_links(link):
+  while True: 
     res = requests.get(link)
     soup = BeautifulSoup(res.text,"lxml")
+    print('this is the soup', soup)
     titles = [urljoin(url,items.get("href")) for items in soup.find_all("a", {"class": "item-title"})]
-    return titles
 
-def get_driver():
-    driver = getattr(threadLocal, 'driver', None)
-    if driver is None:
-        driver = Driver()
-        setattr(threadLocal, 'driver', driver)
-    return driver.driver
+    if len(titles) != 0:
+      break
+  
+  return titles
 
 def get_stock_and_price(url):
-    driver = get_driver()
     driver.get(url)
     sauce = BeautifulSoup(driver.page_source,"lxml")
-    if sauce.select_one(".product-title"):
-        itemName = sauce.select_one(".product-title").text
-        price = sauce.select_one(".price-current").text
-        image = sauce.select_one(".product-view-img-original")['src']
-        if sauce.select_one(".btn-primary"):
-            itemStock = "IN STOCK"
-        else:
-            itemStock = "OUT OF STOCK" 
-    else:
-        itemName = sauce.select_one("h1").text
-        price = sauce.select_one(".current").text.replace('\n', '').strip('Now:  ')
-        image = sauce.select_one(".checkedimg")['src']
-        if sauce.select_one(".atnPrimary").text == "ADD TO CART":
-            itemStock = "IN STOCK"
-        else:
-            itemStock = "OUT OF STOCK"
+    try:
+      if sauce.select_one(".product-title") != None:
+          print("I am not a combo")
+          itemName = sauce.select_one(".product-title").text
+          price = sauce.select_one(".price-current").text
+          image = sauce.select_one(".product-view-img-original")['src']
+          if sauce.select_one(".btn-primary"):
+              itemStock = "IN STOCK"
+          else:
+              itemStock = "OUT OF STOCK" 
+      else:
+          itemName = sauce.select_one("h1").text
+          price = sauce.select_one(".current").text.replace('\n', '').strip('Now:  ')
+          #image = sauce.select_one(".checkedimg")['src']
+          if sauce.select_one(".atnPrimary").text == "ADD TO CART":
+              itemStock = "IN STOCK"
+          else:
+              itemStock = "OUT OF STOCK"
+    except (AttributeError, TypeError) as e:
+      print("Item url: ", url)
+      print("Element could not be found: ", e)
+      return ""
+    
     itemData = {"name": itemName, "price": price, "stock": itemStock, "link": url, "image": image}
-    requests.post("https://warm-ridge-24483.herokuapp.com/product", data=itemData)
     print(f'{itemName}, Stock: {itemStock}, Price: {price}, Link: {url}, Image: {image}\n')
 
+    return itemData
+
 if __name__ == '__main__':
-    while True:
-        threadLocal = threading.local()
-        url = "https://www.newegg.com/p/pl?d=3060+ti"
-        ThreadPool(2).map(get_stock_and_price,get_links(url))
-        
-        del threadLocal
-        import gc
-        gc.collect() # a little extra insurance
-        time.sleep(10)
+    url = "https://www.newegg.com/p/pl?d=3060+ti"
+    links = get_links(url)
+    productData = []
+
+    for link in links:
+      item = get_stock_and_price(link)
+      if type(item) is dict:
+        productData = []
+        print("One item appended")
+    
+    with Pool(5) as p:
+      p.map(post_product, productData)
+    
+
+
 
