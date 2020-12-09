107766c4a6c9a1ec620201c2734233fb6725b77d
elyskrie21 <esamkriegel@gmail.com>
2020-12-08 21:53:45 -0600

    added scheduler

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import requests
 from requests.structures import CaseInsensitiveDict
 import time
 from urllib.parse import urljoin
-from multiprocessing import Pool
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import threading
 import os
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
 
 options = webdriver.ChromeOptions()
-#options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
 options.add_argument("--headless")
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
 
 def post_product(data):
   url = "https://warm-ridge-24483.herokuapp.com/product"
@@ -30,7 +31,6 @@ def get_links(link):
   while True: 
     res = requests.get(link)
     soup = BeautifulSoup(res.text,"lxml")
-    print('this is the soup', soup)
     titles = [urljoin(url,items.get("href")) for items in soup.find_all("a", {"class": "item-title"})]
 
     if len(titles) != 0:
@@ -42,28 +42,6 @@ def get_stock_and_price(url):
     driver.get(url)
     sauce = BeautifulSoup(driver.page_source,"lxml")
     try:
-<<<<<<< HEAD
-        if sauce.select_one(".product-title"):
-            itemName = sauce.select_one(".product-title").text
-            price = sauce.select_one(".price-current").text
-            image = sauce.select_one(".product-view-img-original")['src']
-            if sauce.select_one(".btn-primary"):
-                itemStock = "IN STOCK"
-            else:
-                itemStock = "OUT OF STOCK" 
-        else:
-            itemName = sauce.select_one("h1").text
-            price = sauce.select_one(".current").text.replace('\n', '').strip('Now:  ')
-            image = sauce.select_one(".checkedimg")['src']
-            if sauce.select_one(".atnPrimary").text == "ADD TO CART":
-                itemStock = "IN STOCK"
-            else:
-                itemStock = "OUT OF STOCK"
-    except AttributeError:
-        print('product not found') 
-    itemData = {"name": itemName, "price": int(price), "stock": itemStock, "link": url, "image": image}
-    requests.post("https://warm-ridge-24483.herokuapp.com/product", data=itemData)
-=======
       if sauce.select_one(".product-title") != None:
           print("I am not a combo")
           itemName = sauce.select_one(".product-title").text
@@ -74,25 +52,27 @@ def get_stock_and_price(url):
           else:
               itemStock = "OUT OF STOCK" 
       else:
+          print("I am a combo")
           itemName = sauce.select_one("h1").text
           price = sauce.select_one(".current").text.replace('\n', '').strip('Now:  ')
-          #image = sauce.select_one(".checkedimg")['src']
+          image = sauce.select_one("#mainSlide_0")['src']
           if sauce.select_one(".atnPrimary").text == "ADD TO CART":
               itemStock = "IN STOCK"
           else:
               itemStock = "OUT OF STOCK"
     except (AttributeError, TypeError) as e:
-      print("Item url: ", url)
+      print("Failed item url: ", url)
       print("Element could not be found: ", e)
       return ""
     
     itemData = {"name": itemName, "price": price, "stock": itemStock, "link": url, "image": image}
->>>>>>> 5cec666bbe938583ab822888da15fc2d82cacc2a
     print(f'{itemName}, Stock: {itemStock}, Price: {price}, Link: {url}, Image: {image}\n')
 
     return itemData
 
-if __name__ == '__main__':
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job:
     url = "https://www.newegg.com/p/pl?d=3060+ti"
     links = get_links(url)
     productData = []
@@ -100,12 +80,8 @@ if __name__ == '__main__':
     for link in links:
       item = get_stock_and_price(link)
       if type(item) is dict:
-        productData = []
-        print("One item appended")
+        post_product(item)
+        
+sched.start()
+  
     
-    with Pool(5) as p:
-      p.map(post_product, productData)
-    
-
-
-
